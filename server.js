3e9bee1ec80057747af7d6715ac9e975e44ff3c1
Uniaut <kunwoo0927@gmail.com>
2022-12-06 02:33:50 +0900

    test

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var session = require('express-session');
+var fs = require('fs');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+
+var server = app.listen(8080, function(){
+    console.log("Express server has started on port 8080")
+});
+
+app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(session({
+    secret: '@#@$MYSIGN#@$#$',
+    resave: false,
+    saveUninitialized: true
+}));
+
+var router = require('./router/main')(app, fs);
