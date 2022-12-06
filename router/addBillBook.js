451a5e082a8e9081c284fb62e713010469b5bfd3
zzz664 <zzz664>
2022-12-07 01:27:25 +0900

    addBillBook 라우터 중복된 코드가 생성되면 다시 생성되도록, POST요청으로 바꿈, 최소한의 빌북형태만 추가

--- router/addBillBook.js
+++ router/addBillBook.js
@@ -1,50 +1,36 @@
 module.exports = (app, fs) => {
-    app.get("/addBillBook", (req, res) => {
-      const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
-      const stringLength = 6;
-      let randomCode = "";
+  app.post("/addBillBook", (req, res) => {
+    const dataBuffer = fs.readFileSync("./data/billbook.json");
+    const dataJSON = dataBuffer.toString();
+    const currentBillBookJson = JSON.parse(dataJSON);
+
+    const chars =
+      "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+    const stringLength = 6;
+    let randomCode = "";
+
+    do {
+      randomCode = "";
+
       for (let i = 0; i < stringLength; i++) {
         const rnum = Math.floor(Math.random() * chars.length);
         randomCode += chars.substring(rnum, rnum + 1);
       }
-  
-      const dataType = { };
-      dataType[randomCode] = {
-          "billBookName": "BillBook이름",
-          "billBookDetail": "설명",
-          
-          "bills": [
-              {
-                  "billTitle": {
-                      "date": "날짜",
-                      "time": "시간",
-                      "title": "제목"
-                  },
-                  "summary": "요약글",
-                  "fullPrice": "총가격",
-                  "memberPrice": [
-                      {
-                          "member": "이름",
-                          "price": "가격"
-                      }
-                  ]
-              }
-          ],
-          "members": [
-              {
-                  "name": "사람이름",
-                  "account": {
-                      "bank": "은행이름",
-                      "number": "계좌번호"
-                  }
-              }
-          ]
-      }
-  
-      const dataBuffer = fs.readFileSync('./data/billbook.json');
-      const dataJSON = dataBuffer.toString();
-      const currentBillBookJson = JSON.parse(dataJSON);
-      currentBillBookJson.billbooks.push(dataType);
-      fs.writeFileSync('./data/billbook.json', JSON.stringify(currentBillBookJson));
-    });
-  };
+    } while (currentBillBookJson.billbooks.includes(randomCode) == true);
+
+    const dataType = {};
+    dataType[randomCode] = {
+      billBookName: req.body.billBookName,
+      billBookDetail: req.body.billBookDetail,
+      bills: [],
+      members: [],
+    };
+    currentBillBookJson.billbooks.push(dataType);
+    fs.writeFileSync(
+      "./data/billbook.json",
+      JSON.stringify(currentBillBookJson)
+    );
+
+    res.send({ router: "/addBillBook", success: true });
+  });
+};
